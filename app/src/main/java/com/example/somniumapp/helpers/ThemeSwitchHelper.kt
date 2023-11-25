0d2b3175aae8534f85d39a1c0307d3e15fab7ba7
Radon245 <radon24code@gmail.com>
2023-11-25 21:25:37 +0300

    create ThemeSwitchHelper

---
+++ app/src/main/java/com/example/somniumapp/helpers/ThemeSwitchHelper.kt
@@ -0,0 +1,15 @@
+import android.content.SharedPreferences
+import com.example.somniumapp.R
+
+object ThemeHelper {
+    fun setThemeOfApp(sharedPrefs: SharedPreferences): Int {
+        return when (sharedPrefs.getString("theme_preference", "light")) {
+            "light" -> R.style.AppTheme
+            "dark" -> R.style.AppTheme_Night
+            "green" -> R.style.AppTheme_Holo_green_dark
+            "red" -> R.style.AppTheme_Red
+            "purple" -> R.style.AppTheme_Purple
+            else -> R.style.AppTheme
+        }
+    }
+}
