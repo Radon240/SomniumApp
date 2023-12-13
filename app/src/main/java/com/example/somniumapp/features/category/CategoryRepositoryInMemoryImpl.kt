6506f4d56f34b45ecc585d995dfd10c920ab82c8
Radon245 <radon24code@gmail.com>
2023-12-13 08:54:51 +0300

    override some funs

--- app/src/main/java/com/example/somniumapp/features/category/CategoryRepositoryInMemoryImpl.kt
+++ app/src/main/java/com/example/somniumapp/features/category/CategoryRepositoryInMemoryImpl.kt
@@ -1,5 +1,8 @@
 package com.example.somniumapp.features.category
 
+import com.example.somniumapp.features.category.model.CreateCategoryResponse
+import com.example.somniumapp.features.category.model.DeleteCategoryResponse
+import com.example.somniumapp.features.category.model.EditCategoryResponse
 import com.example.somniumapp.features.category.model.GetCategoriesResponse
 
 class CategoryRepositoryInMemoryImpl : CategoryRepository {
@@ -10,4 +13,18 @@ class CategoryRepositoryInMemoryImpl : CategoryRepository {
             GetCategoriesResponse("Enchantments", "Gameplay")
         )
     }
+
+    override fun createCategory(name: String): CreateCategoryResponse? {
+        return CreateCategoryResponse("Category 1", null)
+    }
+
+    override fun deleteCategory(name: String): DeleteCategoryResponse? {
+        return DeleteCategoryResponse("Category 1")
+    }
+
+    override fun editCategory(name: String): EditCategoryResponse? {
+        return EditCategoryResponse("Category 1", null)
+    }
+
+
 }
