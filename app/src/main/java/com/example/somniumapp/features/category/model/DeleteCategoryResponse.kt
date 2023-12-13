878420b51652f0d5784246ea7f233a69853d1ab5
Radon245 <radon24code@gmail.com>
2023-12-13 08:47:14 +0300

    new Article/Category responses

---
+++ app/src/main/java/com/example/somniumapp/features/category/model/DeleteCategoryResponse.kt
@@ -0,0 +1,5 @@
+package com.example.somniumapp.features.category.model
+
+data class DeleteCategoryResponse(
+    var name: String
+)
---
+++ app/src/main/java/com/example/somniumapp/features/category/model/EditCategoryResponse.kt
@@ -0,0 +1,6 @@
+package com.example.somniumapp.features.category.model
+
+data class EditCategoryResponse(
+    var name:String,
+    var parentName: String?
+)
