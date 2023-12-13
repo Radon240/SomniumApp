a79961a86f9d3317396d43017d2499dded440c5a
Radon245 <radon24code@gmail.com>
2023-12-13 08:51:54 +0300

    added new map of articles by categories

--- app/src/main/java/com/example/somniumapp/features/article/ArticleRepositoryInMemoryImpl.kt
+++ app/src/main/java/com/example/somniumapp/features/article/ArticleRepositoryInMemoryImpl.kt
@@ -1,5 +1,7 @@
 package com.example.somniumapp.features.article
 
+import com.example.somniumapp.features.article.model.CreateArticleResponse
+import com.example.somniumapp.features.article.model.DeleteArticleResponse
 import com.example.somniumapp.features.article.model.GetArticleResponse
 import com.example.somniumapp.features.article.model.GetArticleRevisionsResponse
 import com.example.somniumapp.features.article.model.GetArticlesByCategoryResponse
@@ -8,26 +10,53 @@ import java.time.LocalDateTime
 class ArticleRepositoryInMemoryImpl : ArticleRepository {
     override fun getArticle(title: String): GetArticleResponse? {
         return listOf(
-            GetArticleResponse("a", "test1", "div2005", LocalDateTime.now(), listOf())
+            GetArticleResponse("a", "test content", "div2005", LocalDateTime.now(), listOf())
         ).find {
-            e -> e.title == title
+                e -> e.title == title
         }
     }
-
-    override fun getArticleByCategory(name: String): List<GetArticlesByCategoryResponse>? {
-        return when (name){
-            "" -> listOf(GetArticlesByCategoryResponse("b"))
-            else -> null
-        }
+    override fun deleteArticle(title: String): DeleteArticleResponse? {
+        return deleteArticle("a")
     }
 
-    override fun getArticleByRevision(title: String, revisionId: String): GetArticleResponse? {
-        return listOf(
+    override fun createArticle(): CreateArticleResponse? {
+        return CreateArticleResponse("test")
+    }
 
+    override fun getArticles(category: String): List<GetArticlesByCategoryResponse>?{
+        val articlesByCategory = mapOf(
+            "FAQ" to listOf(
+                GetArticlesByCategoryResponse("Статья 1"),
+                GetArticlesByCategoryResponse("Статья 2")
+            ),
+            "Gameplay" to listOf(
+                GetArticlesByCategoryResponse("В ожидании новогоднего Ивента"),
+                GetArticlesByCategoryResponse("Мыши зополонили спавн"),
+                GetArticlesByCategoryResponse("Правила"),
+                GetArticlesByCategoryResponse("В ожидании новогоднего Ивента"),
+                GetArticlesByCategoryResponse("Мыши зополонили спавн"),
+                GetArticlesByCategoryResponse("Правила"),
+                GetArticlesByCategoryResponse("В ожидании новогоднего Ивента"),
+                GetArticlesByCategoryResponse("Мыши зополонили спавн"),
+                GetArticlesByCategoryResponse("Правила"),
+                GetArticlesByCategoryResponse("В ожидании новогоднего Ивента"),
+                GetArticlesByCategoryResponse("Мыши зополонили спавн"),
+                GetArticlesByCategoryResponse("Правила"),
+                GetArticlesByCategoryResponse("В ожидании новогоднего Ивента"),
+                GetArticlesByCategoryResponse("Мыши зополонили спавн"),
+                GetArticlesByCategoryResponse("Правила"),
+                GetArticlesByCategoryResponse("В ожидании новогоднего Ивента"),
+                GetArticlesByCategoryResponse("Мыши зополонили спавн"),
+                GetArticlesByCategoryResponse("Правила"),
+                GetArticlesByCategoryResponse("В ожидании новогоднего Ивента"),
+                GetArticlesByCategoryResponse("Мыши зополонили спавн"),
+                GetArticlesByCategoryResponse("Правила")
+            ),
+            "Donate" to listOf(
+                GetArticlesByCategoryResponse("Как купить проходку"),
+                GetArticlesByCategoryResponse("Как бесплатно получить админку")
+            )
         )
-    }
-
-    override fun getArticleRevisions(title: String): List<GetArticleRevisionsResponse>? {
-        TODO("Not yet implemented")
+        return articlesByCategory[category]
     }
 }
