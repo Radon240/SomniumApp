2de703889728f596f511063e0a1ce90897673c81
Radon245 <radon24code@gmail.com>
2023-12-13 08:56:00 +0300

    move & setup

--- app/src/main/java/com/example/somniumapp/features/article/ArticleAdapter.kt
+++ app/src/main/java/com/example/somniumapp/features/article/ArticleAdapter.kt
@@ -1,12 +1,22 @@
+package com.example.somniumapp.features.article
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.somniumapp.R
+import com.example.somniumapp.databinding.ArticleItemBinding
 import com.example.somniumapp.features.article.model.GetArticleResponse
+import com.example.somniumapp.features.article.model.GetArticlesByCategoryResponse
 
-class ArticleAdapter(private val articles: List<GetArticleResponse>) : RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>() {
+class ArticleAdapter(private val articles: List<GetArticlesByCategoryResponse>) : RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>() {
+    class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = ArticleItemBinding.bind(itemView)
+        fun bind(article: GetArticlesByCategoryResponse) = with(binding){
+            articleTitle.text = article.title
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.article_item, parent, false)
@@ -14,17 +24,10 @@ class ArticleAdapter(private val articles: List<GetArticleResponse>) : RecyclerV
     }
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
-        val currentArticle = articles[position]
-        holder.titleTextView.text = currentArticle.title
-        // Заполните другие элементы данных внутри article_item.xml, если они есть
+        holder.bind(articles[position])
     }
 
     override fun getItemCount(): Int {
         return articles.size
-    }
-
-    class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val titleTextView: TextView = itemView.findViewById(R.id.textViewTitle)
-        // Добавьте другие элементы интерфейса из article_item.xml, если они есть
     }
 }
